3618f29be1d320fe135ad8d1880effef2a214247
Yosifov <k.yosifov@gmail.com>
2019-04-15 23:20:10 +0300

    Add NxN Matrix Task for Methods Exercise

---
+++ Methods/NxNMatrix.cs
@@ -0,0 +1,25 @@
+﻿namespace TechFundamentals.Methods
+{
+    using System;
+
+    class NxNMatrix
+    {
+        public static void Execute()
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintNxNMatrix(n);
+        }
+
+        private static void PrintNxNMatrix(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    Console.Write(n + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -24,6 +24,7 @@
             //Methods.PasswordValidator.Exercise();
             //Methods.AddAndSubstract.Execute();
             //Methods.MiddleCharacters.Execute();
+            Methods.NxNMatrix.Execute();
         }
     }
 }
